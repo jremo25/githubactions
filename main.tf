a07fc48a29e12418769acb8677dbd0be166a0526
jremo25 <jremo25@outlook.com>
2024-05-04 19:22:24 +0100

    Updated TF files

--- main.tf
+++ main.tf
@@ -1,17 +1,26 @@
-
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "5.27.0"
     }
+  }
+
+  backend "gcs" {
+    bucket         = "statebucket301"
+    prefix         = "terraform/state"
+    credentials    = jsondecode(var.gcp_creds)
   }
 }
 
 provider "google" {
-  # Configuration options
-  credentials = "inner-replica-417201-419c56bdc169.json"
-  region = "europe-west2"
-  project = "inner-replica-417201"
+  region      = "europe-west2"
+  project     = "inner-replica-417201"
+  credentials = jsondecode(var.gcp_creds)
 }
 
+variable "gcp_creds" {
+  description = "GCP Credentials JSON"
+  type        = string
+  sensitive   = true
+}
